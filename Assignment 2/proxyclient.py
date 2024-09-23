2fc22ef7b1753d14d477588f953f48da6336c716
waritsanant06 <waritsanant.ra@gmail.com>
2024-09-23 15:55:50 +0200

    linkoping passed

--- Assignment 2/proxyclient.py
+++ Assignment 2/proxyclient.py
@@ -1,13 +1,13 @@
 import socket
 
-TEST_SERVER = "http://zebroid.ida.liu.se/fakenews/test1.txt"
-
 PORT = 80
 
 def proxyClientSendRequest(request):
+
     """
     forwards http request to internet
     """
+
     request_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     request_str = request.decode()
@@ -27,29 +27,36 @@ def proxyClientSendRequest(request):
 
 def get_server_chunk(socket):
 
-    chunk = socket.recv(2048)
+    chunk = socket.recv(2048).decode('utf-8')
+
 
     print("SERVER RESPONSE:")
     print(chunk)
+    print("--------------------------------")
+
 
     return chunk
 
 
 def proxyClientAlterCont(response, trailing_chars=""):
-    """
-    insert fake news
-    """
+    print("altering content....")
+
+    # """
+    # insert fake news
+    # """
     
     new_response = trailing_chars + response
-    new_response = new_response.replace("Stockholm", "Linkoping")
+    new_response = new_response.replace("Stockholm", "Linköping")
 
     new_response = new_response.replace("smiley", "trolly")
     new_response = new_response.replace("Smiley", "Trolly")
 
-    new_response = new_response.split(" ")
-    new_trailing_chars = new_response[-1]
+    # new_response = new_response.split(" ")
+    # new_trailing_chars = new_response[-1]
 
-    del new_response[-1]
-    new_response = b' '.join(new_response)
+    # del new_response[-1]
+    # new_response = b' '.join(new_response)
 
-    return new_response, new_trailing_chars
+    # return new_response, new_trailing_chars
+    print(f'new response = {new_response}')
+    return new_response,trailing_chars
--- Assignment 2/proxyserver.py
+++ Assignment 2/proxyserver.py
@@ -3,30 +3,25 @@ import proxyclient
 
 PORTNR = 54321
 
-# receive server request from (user) client
-
-# print request
-
-# (debug) return some value
-
-# send request to (proxy) client
 
 def proxyserverstart():
     """
     initiates proxy server, starts listening
     """
-    server_sock = socket.socket()
+    server_socket = socket.socket()
 
-    server_sock.bind(('127.0.0.1', PORTNR))
-    server_sock.listen(5)
-    print("server listening")
+    server_socket.bind(('127.0.0.1', PORTNR))
+    server_socket.listen(5)
+    print(f"server listening on port {PORTNR}")
 
     while True: 
-        client_sock, addr = server_sock.accept()
+
+        client_sock, address = server_socket.accept()
         request = client_sock.recv(4096)
 
         print("BROWSER REQ:")
         print(request)
+        print("--------------------------------")
 
         request_socket = proxyclient.proxyClientSendRequest(request)
         send_data(request_socket, client_sock)
@@ -39,19 +34,26 @@ def send_data(fro, to):
     
 
     chunk = proxyclient.get_server_chunk(fro)
-
-    cont_type_pos = chunk.lower().find("content-type:")
+    print(f'chunk = \n\n <{chunk}>\n\n')
+    cont_type_pos = chunk.lower().find("content-type:") 
+    print(f"content type pos = {cont_type_pos}")   
     cont_type = chunk[cont_type_pos:].split()[1].split("/")[0]
+    print(f'content type = {cont_type}')
+
     temp=""
-    while len(chunk) != 0:
+    
+    while True:
+        if len(chunk) == 0:
+            print("chunk is empty")
+            break
         if cont_type != "image":
             out, temp = proxyclient.proxyClientAlterCont(chunk, temp)
-            to.send(out)
+            to.send(out.encode('utf-8'))
         else:
-            to.send(chunk)
+            to.send(chunk.encode('utf-8'))
         chunk = proxyclient.get_server_chunk(fro)
 
-    to.send(" " + temp)
+    to.send((" " + temp).encode('utf-8'))
 
 
 
